602c1b080dc854b896c14feb8f3d067d46d829fd
amrita-20 <sona20dubey23@gmail.com>
2023-04-26 10:52:12 -0700

    adding intial boiler plate code of final project

--- Final Project/DigitRecognition/src/main/java/module-info.java
+++ Final Project/DigitRecognition/src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.digitrecognition {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.desktop;
+    requires libtensorflow;
+    requires javafx.swing;
+
+
     opens com.digitrecognition to javafx.fxml;
     exports com.digitrecognition;
 }
